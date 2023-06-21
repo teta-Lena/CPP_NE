9f7bdd47caf56851c08f03b65dafac9d56b3171d
Teta_Lena <tetalenaa@gmail.com>
2023-06-21 10:56:12 +0200

    slved bugs

--- cppfinal.cpp
+++ cppfinal.cpp
@@ -37,3 +37,71 @@ string toLower(string &text)
     }
     return texttolower;
 }
+vector<string> split(string userRequest)
+{
+    stringstream lineBuffer(userRequest);
+    string option;
+    vector<string> options;
+    while (getline(lineBuffer, option, ' '))
+    {
+        options.push_back(option);
+    }
+
+    return options;
+}
+bool isAddItemCommandValid(vector<string> commandOptions)
+{
+    if (commandOptions.size() < 5)
+    {
+        return false;
+    }
+    return true;
+}
+void addItem(string id, string name, string quantity, string date)
+{
+    ofstream file;
+    file.open("inventory.csv", ios::app);
+    if (file.is_open())
+    {
+        file << id << "," << name << "," << quantity << "," << date << endl;
+        cout << "" << endl;
+        cout << "Item addded successfully" << endl;
+    }
+    else
+    {
+        cout << "Failed to open file" << endl;
+    }
+};
+int main()
+{
+    string userRequest;
+
+    string userRequest_toLower;
+
+    getStarted();
+
+    getline(cin, userRequest);
+
+    userRequest_toLower = toLower(userRequest);
+    vector<string> commandOptions = split(userRequest_toLower);
+
+    if (commandOptions.at(0) == "additem")
+    {
+        if (isAddItemCommandValid(commandOptions))
+        {
+            {
+                string item_id = commandOptions.at(1);
+                string item_name = commandOptions.at(2);
+                string item_quantity = commandOptions.at(3);
+                string item_registration_date = commandOptions.at(4);
+
+                addItem(item_id, item_name, item_quantity, item_registration_date);
+            }
+            cout << "It is a valid command" << endl;
+        }
+        else
+        {
+            cout << "Invalid command" << endl;
+        }
+    }
+}
